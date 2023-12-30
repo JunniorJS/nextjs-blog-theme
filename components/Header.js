77c45056d1ce4af3275d34994bbd2efc5ba88d3c
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2023-12-30 01:57:29 +0000

    teste

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
+    <header className="pt-20 pb-8">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4" />
       {/* <p className="text-2xl dark:text-white text-center">
         <Link href="/">
