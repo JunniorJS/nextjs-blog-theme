cd99c5c1b577d7ec9366b7c057ef5d5b6a02c3a7
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2023-12-30 01:55:57 +0000

    teste

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,11 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4" />
-      <p className="text-2xl dark:text-white text-center">
+      {/* <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
         </Link>
-      </p>
+      </p> */}
     </header>
   );
 }
