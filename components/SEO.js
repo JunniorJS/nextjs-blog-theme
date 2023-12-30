496d5b9c03ac22ce14b0a776c6cc22462ccbc775
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2023-12-30 03:37:17 +0000

    teste

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,8 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5656727977633233"
+     crossorigin="anonymous"></script>
     </Head>
   );
 }
