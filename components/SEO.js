1cba3c36c3c90ff36f3cfc0c127c280e7f0f022b
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2023-12-30 04:28:46 +0000

    script adsense

--- components/SEO.js
+++ components/SEO.js
@@ -7,6 +7,8 @@ export default function SEO({ title, description }) {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta name="google-adsense-account" content="ca-pub-5656727977633233"></meta>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5656727977633233"
+     crossorigin="anonymous"></script>
     </Head>
   );
 }
