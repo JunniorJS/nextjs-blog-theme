b02ff6db2f2ea53a10fd7fac1ae83ce0e6508c19
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2024-03-17 19:33:40 -0300

    Atualizar o SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,9 +6,6 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      <meta name="google-adsense-account" content="ca-pub-5656727977633233"></meta>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5656727977633233"
-     crossorigin="anonymous"></script>
     </Head>
   );
 }
