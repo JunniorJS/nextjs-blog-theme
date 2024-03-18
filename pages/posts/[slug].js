8c908723fc2de897cea43d9edec2ed20343bfa38
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2024-03-17 21:46:14 -0300

    thkghk

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -49,7 +49,7 @@ export default function PostPage({
             <p className="text-xl mb-4">{frontMatter.description}</p>
           )}
         </header>
-        <main>
+        <main className='' style={ { margin: '0 auto' } }>
           <article className="prose dark:prose-dark">
             <MDXRemote {...source} components={components} />
           </article>
