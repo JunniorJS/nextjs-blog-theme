4720d4de45eda89a97351dc1adc1e0db2d7f2f30
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2024-03-18 06:14:54 -0300

    Atualizar o SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -9,6 +9,7 @@ export default function SEO({ title, description }) {
       <meta property="og:title" content={title} />
       <meta name="keywords" content={'centralizando site, centralizando, centralizando.site, centralizando site de notícias, centralizando site oficial, centralizando brasil'} />
       <link rel="icon" href={'https://i.imgur.com/X5lpQrM.jpeg'} type="image/x-icon" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     </Head>
   );
 }
