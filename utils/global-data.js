443a674b25b488af1004f79bf25f2dcf6fad9c23
JunniorJS <141581018+JunniorJS@users.noreply.github.com>
2024-03-19 18:09:35 -0300

    teste2

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,18 +1,17 @@
 export const getGlobalData = () => {
-  const name = 'Centralizando';
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
+    : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Centralizando: A sua fonte centralizada de Notícias';
+    : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
-    const tags = 'centralizando site, centralizando, centralizando.site, centralizando site de notícias, centralizando site oficial, centralizando brasil'
-    const favicon = 'https://i.imgur.com/X5lpQrM.jpeg'
+
   return {
     name,
     blogTitle,
     footerText,
-    tags,
-    favicon,
   };
 };
